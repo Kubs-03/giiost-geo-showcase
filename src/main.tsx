7b355f188e8b090f1341cbf45fb74491de356314
ushabhagyasrik <ushabhagyasrik@gmail.com>
2025-09-17 18:28:01 +0000

    Fix client-side routing for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/giiost-geo-showcase/">
     <App />
   </BrowserRouter>
 );
